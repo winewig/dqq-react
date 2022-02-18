5c8319d2b0cd5048bc6bc17e2214da4b7ecc535c
unknown <winewig@hotmail.com>
2022-02-18 16:49:58 +0100

    Add media query for header

--- src/component/main/header/header.jsx
+++ src/component/main/header/header.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 import './header.css';
+import logo from './assets/favicon-32x32.png';
 
 const Header = () => {
   return (
     <div className='header'>
       <div className='header-container'>
-        <div>DQQ Family</div>
-        <div>icon</div>
+        <div className='header-logo-and-title'>
+          <img src={logo} alt='logo' />
+          <div className='header-title-container'>
+            <span>DQQ Family</span>
+          </div>
+        </div>
+        <div className='header-icon'>icon</div>
       </div>
     </div>
   );

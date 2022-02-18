9773c86875cab9e9eaab783c67422df6f642dc2d
unknown <winewig@hotmail.com>
2022-02-18 13:50:43 +0100

    Add logo and update the meta

---
+++ src/component/main/header/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './header.css';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-container'>
+        <div>DQQ Family</div>
+        <div>icon</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/component/main/main.jsx
+++ src/component/main/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import Header from './header/header';
 
 export default function Main() {
-  return <div>Main page</div>;
+  return <Header />;
 }
